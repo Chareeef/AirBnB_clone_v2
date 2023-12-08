27ccbb659330bfc8a58d3f233bd1dbf303f21c4b
Chareeef <youssef.charif.h@gmail.com>
2023-12-08 00:45:44 +0000

    Add 100-clean_web_static.py

---
+++ 100-clean_web_static.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+'''
+Fabric tasks to deploy web_static on our servers
+'''
+from fabric.api import local, run, put, env
+from datetime import datetime
+import os
+
+env.hosts = ['52.91.178.165', '18.206.198.119']
+
+
+def do_pack():
+    '''Generates a .tgz archive from the contents of web_static folder'''
+    time_now = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = 'web_static_' + time_now + '.tgz'
+
+    try:
+        local('mkdir -p versions')
+        path = f'versions/{archive_name}'
+        local(f'tar -cvzf {path} web_static')
+        return path
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    '''Distribute an archive to our web servers'''
+
+    try:
+        if not os.path.isfile(archive_path):
+            return False
+        file_path = archive_path.split('/')[-1]  # web_static_xxxxxx.tgz
+        name = file_path.split('.')[0]  # web_static_xxxxxx
+        r_path = '/data/web_static/releases'
+
+        put(archive_path, f'/tmp/{file_path}')
+        run(f'mkdir -p {r_path}/{name}/')
+        run(f'tar -xzf /tmp/{file_path} -C {r_path}/{name}/')
+        run(f'rm /tmp/{file_path}')
+        run(f'mv {r_path}/{name}/web_static/* {r_path}/{name}/')
+        run(f'rm -rf {r_path}/{name}/web_static')
+        run(f'rm -rf /data/web_static/current')
+        run(f'ln -s {r_path}/{name}/ /data/web_static/current')
+        print('New version deployed!')
+        return True
+    except Exception as e:
+        return False
+
+
+def deploy():
+    '''Create and distributes an archive to your web servers'''
+    file_path = do_pack()
+    if file_path is None:
+        return False
+    return do_deploy(file_path)
+
+
+def do_clean(number=0):
+    '''Delete out-of-date archives'''
+
+    n = number
+
+    if n == 0:
+        n = 1
+
+    cp_local = f'cp -f $(ls -c1 versions/ | head -{n} | '
+    cp_local += 'awk \'{print "versions/" $1}\') ./'
+
+    local(cp_local)
+    local('rm -f versions/*')
+    local('mv *.tgz versions/')
+
+    cp_remote = f'cp -rf $(ls -c1 /data/web_static/releases/ | head -{n} | '
+    cp_remote += 'awk \'{print "/data/web_static/releases/" $1}\') /tmp/'
+
+    run(cp_remote)
+    run('rm -rf /data/web_static/releases/*')
+    run('mv /tmp/web_static_* /data/web_static/releases/')
