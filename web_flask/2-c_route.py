e055ae548462838e4395f8c09823270098fd015d
Chareeef <youssef.charif.h@gmail.com>
2023-12-24 16:17:03 +0100

    Add 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,14 +5,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def home():
     return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hhbn():
     return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
 
 
 if __name__ == '__main__':
