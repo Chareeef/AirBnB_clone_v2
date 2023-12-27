4dd17ec5c01806623a91830dfe6d2d7525e58563
Chareeef <youssef.charif.h@gmail.com>
2023-12-27 23:20:31 +0000

    Complete 100-hbnb

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-'''A flask app displaying AirBnB clone's filters'''
+'''A flask app displaying AirBnB clone page'''
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
+from models.user import User
 
 
 app = Flask(__name__)
@@ -15,9 +17,9 @@ def close_session(exception=None):
     storage.close()
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    '''Display an AirBnB clone page with filters feeded by our database'''
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Display the entire AirBnB clone page feeded by our database'''
     states = storage.all(State).values()
     states = list(sorted(states, key=lambda state: state.name))
 
@@ -27,8 +29,18 @@ def hbnb_filters():
     amenities = storage.all(Amenity).values()
     amenities = list(sorted(amenities, key=lambda amenity: amenity.name))
 
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+    places = storage.all(Place).values()
+    places = list(sorted(places, key=lambda place: place.name))
+
+    users = storage.all(User)
+
+    for p in places:
+        owner_key = 'User.' + p.user_id
+        owner = users.get(owner_key)
+        p.owner_name = owner.first_name + ' ' + owner.last_name
+
+    return render_template('100-hbnb.html', states=states,
+                           amenities=amenities, places=places)
 
 
 if __name__ == '__main__':
